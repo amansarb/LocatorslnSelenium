ca5987204d18e168f936ab3c95f97f0bfc74fafc
amansarb <aman.lotey1986@gmail.com>
2020-07-16 10:57:43 +1200

    Add project files.

---
+++ LocatorslnSelenium/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LocatorslnSelenium
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
