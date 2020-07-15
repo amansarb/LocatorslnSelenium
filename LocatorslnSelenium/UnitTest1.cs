3a942aef60aca026d333f6bcf93927415f59c89e
amansarb <aman.lotey1986@gmail.com>
2020-07-16 11:48:24 +1200

    facebook login and signup

--- LocatorslnSelenium/UnitTest1.cs
+++ LocatorslnSelenium/UnitTest1.cs
@@ -1,5 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Drawing;
+using System.Threading;
 namespace LocatorslnSelenium
 {
     [TestClass]
@@ -8,6 +11,35 @@ namespace LocatorslnSelenium
         [TestMethod]
         public void TestMethod1()
         {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Id("email")).SendKeys("aman.lotey@gmail.com");
+            driver.FindElement(By.Id("pass")).SendKeys("aman.lotey@gmail.com");
+            Thread.Sleep(2000);
+            driver.Quit();
+
+
+        }
+        [TestMethod]
+        public void FacebookSignup()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("firstname")).SendKeys("Aman");
+            driver.FindElement(By.Id("lastname")).SendKeys("deep");
+            driver.FindElement(By.Id("lastname")).SendKeys("deep");
+            driver.FindElement(By.Id("lastname")).SendKeys("deep");
+            driver.FindElement(By.Id("lastname")).SendKeys("deep");
+
+
+            Thread.Sleep(2000);
+            driver.Quit();
+
+
         }
     }
 }
