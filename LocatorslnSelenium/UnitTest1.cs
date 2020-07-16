d8eb9f527762ed3d53573d0cb51a400e71a0fc3f
amansarb <aman.lotey1986@gmail.com>
2020-07-16 12:35:20 +1200

    Paragraph tag

--- LocatorslnSelenium/UnitTest1.cs
+++ LocatorslnSelenium/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 using System.Drawing;
 using System.Threading;
 namespace LocatorslnSelenium
@@ -49,9 +50,27 @@ namespace LocatorslnSelenium
 
             driver.Navigate().GoToUrl("https://www.facebook.com/");
             driver.Manage().Window.Maximize();
-            driver.FindElement(By.ClassName("email")).SendKeys("aman.lotey@gmail.com");
-            driver.FindElement(By.ClassName("pass")).SendKeys("aman.lotey@gmail.com");
+            string text=driver.FindElement(By.ClassName("_5iyx")).Text;
+            Console.WriteLine(text);
             Thread.Sleep(2000);
+            driver.Quit();
+
+
+        }
+        [TestMethod]
+        public void tagSelect()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.lipsum.com/");
+            driver.Manage().Window.Maximize();
+
+            var paragraph = driver.FindElements(By.TagName("p"));
+            System.Console.WriteLine(paragraph.Count);
+            Thread.Sleep(2000);
+            foreach (var item in paragraph)
+            { System.Console.WriteLine(item.Text);
+            }
             driver.Quit();
 
 
